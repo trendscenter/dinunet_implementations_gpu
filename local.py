f6c204885d61e43d02348e8fa6c8a7720af62e36
Aashis Khanal <sraashis@gmail.com>
2021-02-11 10:45:54 -0500

    Migrated to coinstac_dinunet.

--- local.py
+++ local.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python
-import itertools
-import json
-import os
-import shutil
 import sys
-from os import sep
-
-import nibabel as ni
+import os
+import torch
 import numpy as np
 import pandas as pd
-import torch
-
-from core import utils as ut
-from core.nn import train_n_eval, init_dataset
-from core.utils import init_k_folds, NNDataset, initialize_weights
+import nibabel as ni
+from coinstac_dinunet import COINNDataset, COINNLocal, COINNTrainer
+import torch.nn.functional as F
+import json
 from models import VBMNet
 
 
@@ -21,98 +15,62 @@ from models import VBMNet
 #
 # pydevd_pycharm.settrace('172.17.0.1', port=8881, stdoutToServer=True, stderrToServer=True, suspend=False)
 
-
-class NiftiDataset(NNDataset):
+class NiftiDataset(COINNDataset):
     def __init__(self, **kw):
         super().__init__(**kw)
-        self.axises = []
-        axises = [0, 1]
-        for i in range(len(axises)):
-            self.axises += list(itertools.combinations(axises, i + 1))
+        self.labels = {}
 
-    def load_indices(self, files, **kw):
-        labels_file = os.listdir(self.label_dir)[0]
-        labels = pd.read_csv(self.label_dir + os.sep + labels_file).set_index('niftifile')
-        for file in files:
-            y = labels.loc[file]['isControl']
-            """
-            int64 could not be json serializable.
-            """
-            self.indices.append([file, int(y)])
+    def load_index(self, site, file):
+        if self.labels.get(site) is None:
+            label_dir = self.path(site, 'label_dir')
+            labels_file = os.listdir(label_dir)[0]
+            self.labels = pd.read_csv(label_dir + os.sep + labels_file).set_index('niftifile')
+        y = self.labels.loc[file]['isControl']
+        self.indices.append([site, file, int(y)])
 
     def __getitem__(self, ix):
-        file, y = self.indices[ix]
-        nif = np.array(ni.load(self.data_dir + sep + file).dataobj)
+        site, file, y = self.indices[ix]
+        data_dir = self.path(site, 'data_dir')
+        nif = np.array(ni.load(data_dir + os.sep + file).dataobj)
         nif[nif < 0.05] = 0
-        # if random.uniform(0, 1) > 0.5:
-        #     nif = np.flip(nif, random.choice(self.axises))
         return {'inputs': torch.tensor(nif.copy()[None, :]), 'labels': torch.tensor(y), 'ix': torch.tensor(ix)}
 
 
-def init_nn(cache, state, init_weights=False):
-    """
-    Initialize neural network/optimizer with locked parameters(check on remote script for that).
-    Also detect and assign specified GPUs.
-    @note Works only with one GPU per site at the moment.
-    """
-    device = ut.get_cuda_device(cache, state)
-    model = VBMNet(in_channels=cache['input_ch'], out_channels=cache['num_class'], init_features=8)
-    optimizer = torch.optim.Adam(model.parameters(), lr=cache['learning_rate'])
-    if init_weights:
-        torch.manual_seed(cache['seed'])
-        initialize_weights(model)
-    return {'device': device, 'model': model.to(device), 'optimizer': optimizer}
+class NiftiTrainer(COINNTrainer):
+    def __init__(self, **kw):
+        super().__init__(**kw)
+
+    def _init_nn_model(self):
+        self.nn['net'] = VBMNet(num_channels=self.cache['input_ch'],
+                                num_classes=self.cache['num_class'],  reduce_by=64)
+
+    def iteration(self, batch):
+        inputs, labels = batch['inputs'].to(self.device['gpu']).float(), batch['labels'].to(self.device['gpu']).long()
+        indices = batch['ix'].to(self.device['gpu']).long()
+
+        out = F.log_softmax(self.nn['net'](inputs), 1)
+        wt = torch.randint(1, 101, (2,)).to(self.device['gpu']).float()
+        loss = F.nll_loss(out, labels, weight=wt)
+
+        _, predicted = torch.max(out, 1)
+        score = self.new_metrics()
+        score.add(predicted, labels)
+        val = self.new_averages()
+        val.add(loss.item(), len(inputs))
+        return {'out': out, 'loss': loss, 'averages': val, 'metrics': score, 'prediction': predicted,
+                'indices': indices}
+
+    def _set_monitor_metric(self):
+        self.cache['monitor_metric'] = 'f1', 'maximize'
+
+    def _set_log_headers(self):
+        self.cache['log_header'] = 'loss,accuracy,f1'
 
 
 if __name__ == "__main__":
-
     args = json.loads(sys.stdin.read())
-    cache = args['cache']
-    input = args['input']
-    state = args['state']
-    out = {}
-
-    nxt_phase = input.get('phase', 'init_runs')
-    if nxt_phase == 'init_runs':
-        """
-        Generate folds as specified. 
-        """
-        cache.update(**input)
-        out.update(**init_k_folds(cache, state))
-        cache['_mode_'] = input['mode']
-        out['mode'] = cache['mode']
-
-    if nxt_phase == 'init_nn':
-        """
-        Initialize neural network/optimizer and GPUs
-        """
-        cache.update(**input['run'][state['clientId']], epoch=0, cursor=0, train_log=[])
-        cache['split_file'] = cache['splits'][str(cache['split_ix'])]
-        cache['log_dir'] = state['outputDirectory'] + sep + cache['id'] + sep + f"fold_{cache['split_ix']}"
-        os.makedirs(cache['log_dir'], exist_ok=True)
-        cache['current_nn_state'] = 'current.nn.pt'
-        cache['best_nn_state'] = 'best.nn.pt'
-        nn = init_nn(cache, state, init_weights=True)
-        ut.save_checkpoint(cache, nn, id=cache['current_nn_state'])
-        init_dataset(cache, state, NiftiDataset)
-        nxt_phase = 'computation'
-
-    if nxt_phase == 'computation':
-        """
-        Train/validation and test phases
-        """
-        nn = init_nn(cache, state, init_weights=False)
-        out_, nxt_phase = train_n_eval(nn, cache, input, state, NiftiDataset, nxt_phase)
-        out.update(**out_)
-
-    elif nxt_phase == 'success':
-        """
-        This phase receives global scores from the aggregator.
-        """
-        shutil.copy(f"{state['baseDirectory']}{sep}{input['results_zip']}.zip",
-                    f"{state['outputDirectory'] + sep + cache['id']}{sep}{input['results_zip']}.zip")
-
-    out['phase'] = nxt_phase
-    output = json.dumps({'output': out, 'cache': cache})
-    sys.stdout.write(output)
-    args, cache, input, state, out, output = [None] * 6
+    local = COINNLocal(cache=args['cache'], input=args['input'],
+                       state=args['state'], epochs=111, patience=21, gpus=[],
+                       pretrain_epochs=21, computation_id='fsv_volumes_pretrained')
+    local.compute(NiftiDataset, NiftiTrainer)
+    local.send()
--- remote.py
+++ remote.py
@@ -1,214 +1,26 @@
 #!/usr/bin/python
-import datetime
-import json
-import os
-import random
-import shutil
 import sys
+import json
 
-import numpy as np
-
-import core.utils
-from core.measurements import Prf1a, Avg
+from coinstac_dinunet import COINNRemote
 
 
+#
 # import pydevd_pycharm
 #
 # pydevd_pycharm.settrace('172.17.0.1', port=8881, stdoutToServer=True, stderrToServer=True, suspend=False)
 
 
-def aggregate_sites_info(input):
-    """
-    Average each sites gradients and pass it to all sites.
-    """
-    out = {}
-    grads = []
-    for site, site_vars in input.items():
-        grad_file = state['baseDirectory'] + os.sep + site + os.sep + site_vars['grads_file']
-        grads.append(np.load(grad_file, allow_pickle=True))
-    out['avg_grads'] = 'avg_grads.npy'
-    avg_grads = []
-    for layer_grad in zip(*grads):
-        avg_grads.append(np.array(layer_grad).mean(0))
-    np.save(state['transferDirectory'] + os.sep + out['avg_grads'], np.array(avg_grads))
-    return out
+class NiftiRemote(COINNRemote):
+    def _set_monitor_metric(self):
+        self.cache['monitor_metric'] = 'f1', 'maximize'
 
-
-def init_runs(cache, input):
-    cache.update(id=[v['id'] for _, v in input.items()][0])
-    cache.update(num_folds=[v['num_folds'] for _, v in input.items()][0])
-    cache.update(seed=[v.get('seed') for _, v in input.items()][0])
-    cache.update(seed=random.randint(0, int(1e6)) if cache['seed'] is None else cache['seed'])
-    cache['folds'] = []
-    for fold in range(cache['num_folds']):
-        cache['folds'].append({'split_ix': fold, 'seed': cache['seed']})
-
-    cache['folds'] = cache['folds'][::-1]
-
-
-def next_run(cache, input, state):
-    """
-    This function pops a new fold, lock parameters, and forward init_nn signal to all sites
-    """
-    cache['fold'] = cache['folds'].pop()
-    cache.update(log_dir=state['outputDirectory'] + os.sep + cache['id'] + os.sep + f"fold_{cache['fold']['split_ix']}")
-    os.makedirs(cache['log_dir'], exist_ok=True)
-
-    cache.update(best_val_score=0)
-    cache.update(train_log=['Loss,Precision,Recall,F1,Accuracy'],
-                 validation_log=['Loss,Precision,Recall,F1,Accuracy'],
-                 test_log=['Loss,Precision,Recall,F1,Accuracy'])
-
-    """**** Parameter Lock ******"""
-    out = {}
-    for site, site_vars in input.items():
-        out[site] = cache['fold']
-    return out
-
-
-def check(logic, k, v, input):
-    phases = []
-    for site_vars in input.values():
-        phases.append(site_vars.get(k) == v)
-    return logic(phases)
-
-
-def on_epoch_end(cache, input):
-    """
-    #############################
-    Entry status: "train_waiting"
-    Exit status: "train"
-    ############################
-
-    This function runs once an epoch of training is done and all sites
-        run the validation step i.e. all sites in "train_waiting" status.
-    We accumulate training/validation loss and scores of the last epoch.
-    We also send a save current model as best signal to all sites if the global validation score is better than the previously saved one.
-    """
-    out = {}
-    train_prfa = Prf1a()
-    train_loss = Avg()
-    val_prfa = Prf1a()
-    val_loss = Avg()
-    for site, site_vars in input.items():
-        for tl, tp in site_vars['train_log']:
-            train_loss.add(tl['sum'], tl['count'])
-            train_prfa.update(tp=tp['tp'], tn=tp['tn'], fn=tp['fn'], fp=tp['fp'])
-        vl, vp = site_vars['validation_log']
-        val_loss.add(vl['sum'], vl['count'])
-        val_prfa.update(tp=vp['tp'], tn=vp['tn'], fn=vp['fn'], fp=vp['fp'])
-
-    cache['train_log'].append([train_loss.average, *train_prfa.prfa()])
-    cache['validation_log'].append([val_loss.average, *val_prfa.prfa()])
-
-    if val_prfa.f1 >= cache['best_val_score']:
-        cache['best_val_score'] = val_prfa.f1
-        out['save_current_as_best'] = True
-    else:
-        out['save_current_as_best'] = False
-    core.utils.save_logs(cache, plot_keys=['train_log', 'validation_log'], log_dir=cache['log_dir'])
-    return out
-
-
-def save_test_scores(cache, input):
-    """
-    ########################
-    Entry: phase "next_run_waiting"
-    Exit: continue on next fold if folds left, else success and stop
-    #######################
-    This function saves test score of last fold.
-    """
-    test_prfa = Prf1a()
-    test_loss = Avg()
-    for site, site_vars in input.items():
-        vl, vp = site_vars['test_log']
-        test_loss.add(vl['sum'], vl['count'])
-        test_prfa.update(tp=vp['tp'], tn=vp['tn'], fn=vp['fn'], fp=vp['fp'])
-    cache['test_log'].append([test_loss.average, *test_prfa.prfa()])
-    cache['test_scores'] = json.dumps(vars(test_prfa))
-    cache['global_test_score'].append(vars(test_prfa))
-    core.utils.save_logs(cache, plot_keys=['train_log', 'validation_log'], file_keys=['test_log', 'test_scores'],
-                         log_dir=cache['log_dir'])
-
-
-def send_global_scores(cache, state):
-    out = {}
-    score = Prf1a()
-    for sc in cache['global_test_score']:
-        score.update(tp=sc['tp'], tn=sc['tn'], fn=sc['fn'], fp=sc['fp'])
-    cache['global_test_score'] = ['Precision,Recall,F1,Accuracy']
-    cache['global_test_score'].append(score.prfa())
-    core.utils.save_logs(cache, file_keys=['global_test_score'],
-                         log_dir=state['outputDirectory'] + os.sep + cache['id'])
-
-    out['results_zip'] = f"{cache['id']}_" + '_'.join(str(datetime.datetime.now()).split(' '))
-    shutil.make_archive(f"{state['transferDirectory']}{os.sep}{out['results_zip']}", 'zip', cache['log_dir'])
-    return out
-
-
-def set_mode(input, mode=None):
-    out = {}
-    for site, site_vars in input.items():
-        out[site] = mode if mode else site_vars['mode']
-    return out
+    def _set_log_headers(self):
+        self.cache['log_header'] = 'loss,accuracy,f1'
 
 
 if __name__ == "__main__":
     args = json.loads(sys.stdin.read())
-    cache = args['cache']
-    input = args['input']
-    state = args['state']
-    out = {}
-
-    nxt_phase = input.get('phase', 'init_runs')
-    if check(all, 'phase', 'init_runs', input):
-        """
-        Initialize all folds and loggers
-        """
-        cache['global_test_score'] = []
-        init_runs(cache, input)
-        out['run'] = next_run(cache, input, state)
-        out['global_modes'] = set_mode(input)
-        nxt_phase = 'init_nn'
-
-    if check(all, 'phase', 'computation', input):
-        """
-        Main computation phase where we aggregate sites information
-        We also handle train/validation/test stages of local sites by sending corresponding signals from here
-        """
-        nxt_phase = 'computation'
-        out['global_modes'] = set_mode(input)
-        if check(all, 'grads_file', 'grads.npy', input):
-            out.update(**aggregate_sites_info(input))
-
-        if check(all, 'mode', 'val_waiting', input):
-            out['global_modes'] = set_mode(input, mode='validation')
-
-        if check(all, 'mode', 'train_waiting', input):
-            out.update(**on_epoch_end(cache, input))
-            out['global_modes'] = set_mode(input, mode='train')
-
-        if check(all, 'mode', 'test', input):
-            out.update(**on_epoch_end(cache, input))
-            out['global_modes'] = set_mode(input, mode='test')
-
-    if check(all, 'phase', 'next_run_waiting', input):
-        """
-        This block runs when a fold has completed all train, test, validation phase.
-        We save all the scores and plot the results.
-        We transition to new fold if there is any left, else we stop the distributed computation with a success signal.
-        """
-        save_test_scores(cache, input)
-        if len(cache['folds']) > 0:
-            out['nn'] = {}
-            out['run'] = next_run(cache, input, state)
-            out['global_modes'] = set_mode(input)
-            nxt_phase = 'init_nn'
-        else:
-            out.update(**send_global_scores(cache, state))
-            nxt_phase = 'success'
-
-    out['phase'] = nxt_phase
-    output = json.dumps({'output': out, 'cache': cache, 'success': check(all, 'phase', 'success', input)})
-    sys.stdout.write(output)
-    args, cache, input, state, out, output = [None] * 6
+    remote = NiftiRemote(cache=args['cache'], input=args['input'], state=args['state'])
+    remote.compute()
+    remote.send()
