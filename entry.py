5ce963338d71690ad3ce287ab341fc453363fa62
Aashis Khanal <sraashis@gmail.com>
2021-08-10 11:12:16 -0400

    Persistent server conversion

--- entry.py
+++ entry.py
@@ -7,7 +7,7 @@ from coinstac_dinunet.metrics import Prf1a
 from classification import NiftiDataset, NiftiTrainer
 
 
-class FSRemote(COINNRemote):
+class VBMRemote(COINNRemote):
     def _set_monitor_metric(self):
         self.cache['monitor_metric'] = 'f1', 'maximize'
 
@@ -23,13 +23,13 @@ class Server(COINPyService):
     def get_local(self, msg) -> callable:
         pretrain_args = {'epochs': 51, 'batch_size': 16}
         local = COINNLocal(cache=self.cache, input=msg['data']['input'],
-                           pretrain_args=None, batch_size=4, model_scale=4,
+                           pretrain_args=None, batch_size=8, model_scale=4,
                            state=msg['data']['state'], epochs=21, patience=21, computation_id='vbm_quick')
         return local
 
     def get_remote(self, msg) -> callable:
-        remote = FSRemote(cache=self.cache, input=msg['data']['input'],
-                          state=msg['data']['state'])
+        remote = VBMRemote(cache=self.cache, input=msg['data']['input'],
+                           state=msg['data']['state'])
         return remote
 
     def get_local_compute_args(self, msg) -> list:
